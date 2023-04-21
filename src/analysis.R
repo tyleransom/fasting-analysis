7257228e8672f5be66e95d21bacf077d9e57286f
Tyler Ransom <tyleransom@gmail.com>
2023-04-21 12:33:15 -0500

    complete(?) analysis

--- src/analysis.R
+++ src/analysis.R
@@ -3,48 +3,118 @@ library(magrittr)
 library(lubridate)
 library(tsibble)
 library(tseries)
-library(car)
-library(estimatr)
+library(fable)
+library(feasts)
+library(dynlm)
 library(modelsummary)
+
+#-------------------------------------------------------------------------------
+# set working directory
+#-------------------------------------------------------------------------------
+this.dir <- dirname(parent.frame(2)$ofile)
+setwd(this.dir)
 
 load('../data/cleaned/daily_fitbit.rda')
 
-#lm(weight ~ sleephrs + I(steps/1e3) + cumul_dur_dec + ate_restaurant + ate_pizza, data = all) %>% summary()
+#-------------------------------------------------------------------------------
+# data cleaning
+#-------------------------------------------------------------------------------
 
-Count <- function(x) sum(!is.na(x))
-datasummary(steps + sleephrs + cumul_dur_dec + day + month ~ Mean + Count, all, output="markdown") %>% print
-all %<>% filter(date > as_date('2019-11-16')) %>% mutate(steps = na_if(steps,0),
-                                                         daynum = day,
-                                                         monthnum = month,
-                                                         dayname = case_when(daynum==1 ~ "Sun", daynum==2 ~ "Mon", daynum==3 ~ "Tue", daynum==4 ~ "Wed", daynum==5 ~ "Thu", daynum==6 ~ "Fri", daynum==7 ~ "Sat"),
-                                                         day = as.factor(dayname),
-                                                         monthname = case_when(month==1 ~ "Jan", month==2 ~ "Feb", month==3 ~ "Mar", month==4 ~ "Apr", month==5 ~ "May", month==6 ~ "Jun", month==7 ~ "Jul", month==8 ~ "Aug", month==9 ~ "Sep", month==10 ~ "Oct", month==11 ~ "Nov", month==12 ~ "Dec"),
-                                                         month = as.factor(monthname))
-all %<>% drop_na(steps,sleephrs,cumul_dur_dec,day,month)
-datasummary(steps + sleephrs + day + month ~ Mean + Count, all, output="markdown") %>% print
+# drop last observation (data corrupt)
+all %<>% slice(-nrow(.))
 
-# is there a unit root?
-adf.test(log(all$steps), k=1) %>% print
-adf.test(log(all$sleephrs), k=1) %>% print
-adf.test(log(all$cumul_dur_dec), k=1) %>% print
-#adf.test(all$weight, k=1) %>% print
+# create "post two meals a day" variable
+all %<>% mutate(post_tmad = case_when(date > as_date('2022-07-24') ~ "Post", TRUE ~ "Pre"))
+post.tmad <- all %>% filter(date >= as_date('2022-07-24'))
 
-asgeag
+#-------------------------------------------------------------------------------
+# simple descriptive questions
+#-------------------------------------------------------------------------------
+# how many days since july 24 2022 have I gotten less than 7k steps?
+ndays <- post.tmad %>% nrow
+nmiss <- post.tmad %>% filter(steps<7000) %>% nrow
+# note: this answer is wrong due to some way of aggregating steps over days in the FitBit API data
+# the correct answer is 7 out of 251
 
-# time series regression
-est1 <- lm_robust(log(sleephrs) ~ lag(log(sleephrs)) + log(steps) + lag(log(steps)), data=all)
-est1a<-        lm(log(sleephrs) ~ lag(log(sleephrs)) + log(steps) + lag(log(steps)), data=all)
-glance(est1a) %>% print
+# how many times per week did I eat out before/after TMAD?
+agg <- all %>% as_tibble %>% group_by(post_tmad) %>% summarize(n_days = n(), n_pizza = sum(ate_pizza), n_restaurant = sum(ate_restaurant), n_ice_cream = sum(ate_ice_cream))
+agg %<>% mutate(
+                n_pizza_week = n_pizza/n_days*7,
+                n_restaurant_week = n_restaurant/n_days*7,
+                n_ice_cream_week = n_ice_cream/n_days*7,
+               )
+agg %>% select(post_tmad,ends_with("week")) %>% print
 
-est2 <- lm_robust(log(sleephrs) ~ lag(log(sleephrs)) + log(steps) + lag(log(steps)), data=all)
-est2a<-        lm(log(sleephrs) ~ lag(log(sleephrs)) + log(steps) + lag(log(steps)), data=all)
-glance(est2a) %>% print
 
-linearHypothesis(est2, c("daySun=0", "dayMon=0", "dayTue=0", "dayWed=0", "dayThu=0", "daySat=0",
-                         "monthAug=0", "monthDec=0", "monthFeb=0", "monthJan=0", "monthJul=0", "monthJun=0", "monthMar=0", "monthMay=0", "monthNov=0", "monthOct=0", "monthSep=0")) %>% print
-linearHypothesis(est2a, c("daySun=0", "dayMon=0", "dayTue=0", "dayWed=0", "dayThu=0", "daySat=0",
-                          "monthAug=0", "monthDec=0", "monthFeb=0", "monthJan=0", "monthJul=0", "monthJun=0", "monthMar=0", "monthMay=0", "monthNov=0", "monthOct=0", "monthSep=0")) %>% print
-((deviance(est1a)-deviance(est2a))/17)/(deviance(est2a)/est2a$df)
+#-------------------------------------------------------------------------------
+# transform time series to be detrended and seasonally adjusted
+#-------------------------------------------------------------------------------
+fasting_adj <- all %>% model(STL(cumul_dur_dec ~ season(window = Inf))) %>%
+               components() %>%
+               select(remainder)
+all %<>% mutate(fasting_adj = fasting_adj$remainder)
 
-modelsummary(list(est1,est2),stars=T,output="markdown") %>% print
+weight_adj <- all %>% model(STL(weight ~ season(window = Inf))) %>%
+              components() %>%
+              select(remainder)
+all %<>% mutate(weight_adj = weight_adj$remainder)
 
+steps_adj <- all %>% model(STL(steps ~ season(window = Inf))) %>% 
+             components() %>% 
+             select(remainder)
+all %<>% mutate(steps_adj = steps_adj$remainder/1000)
+
+sleep_adj <- all %>% model(STL(sleephrs ~ season(window = Inf))) %>% 
+             components() %>% 
+             select(remainder)
+all %<>% mutate(sleep_adj = sleep_adj$remainder)
+
+
+#-------------------------------------------------------------------------------
+# unit root tests
+#-------------------------------------------------------------------------------
+adf.test(all$fasting_adj, k=1) %>% print
+adf.test(all$weight_adj,  k=1) %>% print
+adf.test(all$steps_adj,   k=1) %>% print
+adf.test(all$sleep_adj,   k=1) %>% print
+
+
+#-------------------------------------------------------------------------------
+# create some variables for later analysis
+#-------------------------------------------------------------------------------
+all %<>% mutate(long_fast_adj = fasting_adj>=10)
+
+
+#-------------------------------------------------------------------------------
+# ARMA models
+#-------------------------------------------------------------------------------
+fit <- lm(weight_adj ~ lag(weight_adj)    + lag(weight_adj,2)    + lag(weight_adj,3)    + lag(weight_adj,4)    + lag(weight_adj,5)    + lag(weight_adj,6)    + lag(weight_adj,7)    + 
+                       lag(steps_adj)     + lag(steps_adj,2)     + lag(steps_adj,3)     + lag(steps_adj,4)     + lag(steps_adj,5)     + lag(steps_adj,6)     + lag(steps_adj,7)     + 
+                       lag(sleep_adj)     + lag(sleep_adj,2)     + lag(sleep_adj,3)     + lag(sleep_adj,4)     + lag(sleep_adj,5)     + lag(sleep_adj,6)     + lag(sleep_adj,7)     + 
+                       lag(fasting_adj)   + lag(fasting_adj,2)   + lag(fasting_adj,3)   + lag(fasting_adj,4)   + lag(fasting_adj,5)   + lag(fasting_adj,6)   + lag(fasting_adj,7)   +
+                       lag(long_fast_adj) + lag(long_fast_adj,2) + lag(long_fast_adj,3) + lag(long_fast_adj,4) + lag(long_fast_adj,5) + lag(long_fast_adj,6) + lag(long_fast_adj,7) +
+                       lag(traveling)     + lag(traveling,2)     + lag(traveling,3)     + lag(traveling,4)     + lag(traveling,5)     + lag(traveling,6)     + lag(traveling,7)     +
+                       lag(ate_pizza)     + lag(ate_pizza,2)     + lag(ate_pizza,3)     + lag(ate_pizza,4)     + lag(ate_pizza,5)     + lag(ate_pizza,6)     + lag(ate_pizza,7)     , data=all )
+summary(fit)
+
+fit <- lm(weight_adj ~ lag(weight_adj)    + lag(weight_adj,2)    + lag(weight_adj,3)    + lag(weight_adj,4)    + lag(weight_adj,5)    + lag(weight_adj,6)    + lag(weight_adj,7)    + 
+                       lag(steps_adj)     + lag(steps_adj,2)     + lag(steps_adj,3)     + lag(steps_adj,4)     + lag(steps_adj,5)     + lag(steps_adj,6)     + lag(steps_adj,7)     + 
+                       lag(sleep_adj)     + lag(sleep_adj,2)     + lag(sleep_adj,3)     + lag(sleep_adj,4)     + lag(sleep_adj,5)     + lag(sleep_adj,6)     + lag(sleep_adj,7)     +            
+                       lag(fasting_adj)   + lag(fasting_adj,2)   + lag(fasting_adj,3)   + lag(fasting_adj,4)   + lag(fasting_adj,5)   + lag(fasting_adj,6)   + lag(fasting_adj,7)   +
+                       lag(long_fast_adj) + lag(long_fast_adj,2) + lag(long_fast_adj,3) + lag(long_fast_adj,4) + lag(long_fast_adj,5) + lag(long_fast_adj,6) + lag(long_fast_adj,7) +
+                       lag(traveling)     + lag(traveling,2)     + lag(traveling,3)     + lag(traveling,4)     + lag(traveling,5)     + lag(traveling,6)     + lag(traveling,7)     +
+                       lag(ate_pizza)     + lag(ate_pizza,2)     + lag(ate_pizza,3)     + lag(ate_pizza,4)     + lag(ate_pizza,5)     + lag(ate_pizza,6)     + lag(ate_pizza,7)     , data=all %>% filter(date >= as_date('2022-07-24')))
+summary(fit)
+
+fit <- all %>% model(
+                     ARIMA(
+                           weight_adj ~ lag(weight_adj)    + lag(weight_adj,2)    + lag(weight_adj,3)    + lag(weight_adj,4)    + lag(weight_adj,5)    + lag(weight_adj,6)    + lag(weight_adj,7)    + 
+                                        lag(steps_adj)     + lag(steps_adj,2)     + lag(steps_adj,3)     + lag(steps_adj,4)     + lag(steps_adj,5)     + lag(steps_adj,6)     + lag(steps_adj,7)     + 
+                                        lag(sleep_adj)     + lag(sleep_adj,2)     + lag(sleep_adj,3)     + lag(sleep_adj,4)     + lag(sleep_adj,5)     + lag(sleep_adj,6)     + lag(sleep_adj,7)     +            
+                                        lag(fasting_adj)   + lag(fasting_adj,2)   + lag(fasting_adj,3)   + lag(fasting_adj,4)   + lag(fasting_adj,5)   + lag(fasting_adj,6)   + lag(fasting_adj,7)   +
+                                        lag(long_fast_adj) + lag(long_fast_adj,2) + lag(long_fast_adj,3) + lag(long_fast_adj,4) + lag(long_fast_adj,5) + lag(long_fast_adj,6) + lag(long_fast_adj,7) +
+                                        lag(traveling)     + lag(traveling,2)     + lag(traveling,3)     + lag(traveling,4)     + lag(traveling,5)     + lag(traveling,6)     + lag(traveling,7)     +
+                                        lag(ate_pizza)     + lag(ate_pizza,2)     + lag(ate_pizza,3)     + lag(ate_pizza,4)     + lag(ate_pizza,5)     + lag(ate_pizza,6)     + lag(ate_pizza,7)     
+                          )
+                    )
+report(fit)
--- src/graphs.R
+++ src/graphs.R
@@ -3,18 +3,21 @@ library(magrittr)
 library(lubridate)
 library(tsibble)
 library(fable)
+library(feasts)
 
 load('../data/cleaned/daily_fitbit.rda')
 
+#-------------------------------------------------------------------------------
 # make some figures of weight and fasting duration and steps
+#-------------------------------------------------------------------------------
 # graph of fasting duration
-fasting %>% autoplot(cumul_dur_dec) + 
+all %>% autoplot(cumul_dur_dec) + 
             theme_minimal()+
             xlab("Calendar date") +
             ylab("Fasting duration (hours)") +
             ylim(0, 100) +
             scale_y_continuous(breaks = seq(12, 96, by = 12))
-ggsave('../exhibits/figures/fasting_duration.png')
+ggsave('../exhibits/figures/fasting_duration.png', width = 7, height = 7)
 
 # graph of weight
 all %>% autoplot(weight) + 
@@ -22,17 +25,95 @@ all %>% autoplot(weight) +
         xlab("Calendar date") +
         ylab("Weight (lbs)") +
         ylim(140, 170)
-ggsave('../exhibits/figures/weight.png')
+ggsave('../exhibits/figures/weight.png', width = 7, height = 7)
 
 # graph of steps
-fasting %>% autoplot(steps) + 
+all %>% autoplot(steps) + 
             theme_minimal()+
             xlab("Calendar date") +
             ylab("Steps") +
             ylim(0, 20000) +
             scale_y_continuous(breaks = seq(2000, 20000, by = 2000))
-ggsave('../exhibits/figures/steps.png')
+ggsave('../exhibits/figures/steps.png', width = 7, height = 7)
 
-# see https://fabletools.tidyverts.org/reference/decomposition_model.html
-# ETS try
+# graph of hours slept
+all %>% autoplot(sleephrs) + 
+  theme_minimal()+
+  xlab("Calendar date") +
+  ylab("Hours slept") +
+  ylim(3, 9) +
+  scale_y_continuous(breaks = seq(3, 9, by = 0.5))
+ggsave('../exhibits/figures/sleep.png', width = 7, height = 7)
 
+
+#-------------------------------------------------------------------------------
+# STL decompositions
+#-------------------------------------------------------------------------------
+all %>% 
+  model(STL(cumul_dur_dec ~ season(window = Inf))) %>% 
+  components() %>% 
+  autoplot() +
+  theme_minimal()
+ggsave('../exhibits/figures/fasting_STL.png', width = 12, height = 8, units = "in", dpi = 1200)
+
+all %>% 
+  model(STL(weight ~ season(window = Inf))) %>% 
+  components() %>% 
+  autoplot() +
+  theme_minimal()
+ggsave('../exhibits/figures/weight_STL.png', width = 12, height = 8, units = "in", dpi = 1200)
+
+all %>% 
+  model(STL(steps ~ season(window = Inf))) %>% 
+  components() %>% 
+  autoplot() +
+  theme_minimal()
+ggsave('../exhibits/figures/steps_STL.png', width = 12, height = 8, units = "in", dpi = 1200)
+
+all %>% 
+  model(STL(sleephrs ~ season(window = Inf))) %>% 
+  components() %>% 
+  autoplot() +
+  theme_minimal()
+ggsave('../exhibits/figures/sleep_STL.png', width = 12, height = 8, units = "in", dpi = 1200)
+
+
+#-------------------------------------------------------------------------------
+# plot just the trend component from each STL decomp, all on the same graph
+#-------------------------------------------------------------------------------
+test1 <- all %>% 
+  model(STL(cumul_dur_dec ~ season(window = Inf))) %>% 
+  components() %>% 
+  select(Date=date, Trend=trend) %>%
+  mutate(var = "Fasting Duration (hrs)")
+
+test2 <- all %>% 
+  model(STL(weight ~ season(window = Inf))) %>% 
+  components() %>% 
+  select(Date=date, Trend=trend) %>%
+  mutate(var = "Weight (lbs)")
+
+test3 <- all %>% 
+  model(STL(steps ~ season(window = Inf))) %>% 
+  components() %>% 
+  select(Date=date, Trend=trend) %>%
+  mutate(var = "Steps")
+
+test4 <- all %>% 
+  model(STL(sleephrs ~ season(window = Inf))) %>% 
+  components() %>% 
+  select(Date=date, Trend=trend) %>%
+  mutate(var = "Sleep (hrs)") 
+
+test.long <- bind_rows(as_tibble(test1),
+                       as_tibble(test2),
+                       as_tibble(test3),
+                       as_tibble(test4))
+
+# create the ggplot with facets
+test.long %>%
+  ggplot(aes(x = Date, y = Trend)) +
+  geom_line() +
+  theme_minimal() +
+  facet_wrap(~ var, ncol = 1, scales = "free_y")
+ggsave('../exhibits/figures/all_trends.png', width = 9, height = 6, units = "in", dpi = 1200)
